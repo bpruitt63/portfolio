32915ef44fff20fc3abf6668748e0e2e51236039
Brian Pruitt <brianrpruitt63@gmail.com>
2023-04-05 20:21:25 -0400

    added video embed

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './static/styles/video.css';
 
 function Project({project}) {
+
+    const [video, setVideo] = useState(false);
+
     return (
         <>
             <h4>{project.title}</h4>
             <p>{project.image}</p>
+            <button onClick={() => setVideo(!video)}>
+                {video ? 'Hide Demo Video' : 'View Demo Video'}
+            </button>
+            {video && 
+                <div className='iframeDiv'>
+                    <iframe src={`${project.video}?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true`}
+                            className='iframeVid'
+                            webkitallowfullscreen='true'
+                            mozallowfullscreen='true'
+                            allow='fullscreen'>
+                    </iframe>
+                </div>}
             <p>{project.description}</p>
-            <p>{project.video}</p>
             <p>{project.link}</p>
             {project.backendFiles &&
                 <p>{project.backendFiles}</p>}
@@ -19,3 +34,4 @@ function Project({project}) {
 };
 
 export default Project;
+
--- src/static/project_data.js
+++ src/static/project_data.js
@@ -4,7 +4,7 @@ export const projects = [
         image: 'image',
         description: 'description',
         link: 'https://sporto-app.herokuapp.com/',
-        video: 'video',
+        video: 'https://www.loom.com/embed/befca497d2c443a2bec3e377b9e41c13',
         backendFiles: 'https://github.com/bpruitt63/sporty-backend',
         frontendFiles: 'https://github.com/bpruitt63/sporty-frontend'
     },
@@ -13,7 +13,7 @@ export const projects = [
         image: 'image',
         description: 'description',
         link: 'https://cashcount.herokuapp.com/',
-        video: 'video',
+        video: 'https://www.loom.com/embed/e260c1462962423a8e192d4511bec167',
         backendFiles: 'https://github.com/bpruitt63/cashCount-backend',
         frontendFiles: 'https://github.com/bpruitt63/cashCount-frontend'
     },
@@ -22,7 +22,7 @@ export const projects = [
         image: 'image',
         description: 'description',
         link: 'https://boatey.herokuapp.com/',
-        video: 'video',
+        video: 'https://www.loom.com/embed/38885b03926643e8a1e868a2ccd8fc7a',
         backendFiles: 'https://github.com/bpruitt63/capstone2-backend',
         frontendFiles: 'https://github.com/bpruitt63/capstone2-frontend'
     },
@@ -31,7 +31,7 @@ export const projects = [
         image: 'image',
         description: 'description',
         link: 'https://gamey-project.herokuapp.com/',
-        video: 'video',
+        video: 'https://www.loom.com/embed/2dd8d96dce2c49a9b9c109c1f62d6511',
         singleRepo: 'https://github.com/bpruitt63/capstone1'
     }
 ];
