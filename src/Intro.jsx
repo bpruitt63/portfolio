2e30713626cf2825d344c8545811b44b8a81e840
Brian Pruitt <brianrpruitt63@gmail.com>
2023-04-09 17:41:22 -0400

    added intro font

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './static/styles/intro.css';
 import laptop from './static/images/laptop.png';
 
 function Intro() {
     
     return (
         <div className='intro'>
-            <h1>Brian Pruitt</h1>
+            <h1>Brian<br />Pruitt</h1>
             <img src={laptop} 
                 alt='Laptop'
                 id='laptop' />
