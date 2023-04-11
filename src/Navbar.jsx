100c5cb8d29c3dbbfd9816b48266a7da1ed487ac
Brian Pruitt <brianrpruitt63@gmail.com>
2023-04-10 21:04:08 -0400

    added basic dropdown to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './static/styles/navbar.css';
 
 function Navbar() {
+
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen(!dropdownOpen);
+
     return (
         <div className='navbar'>
             <a href='#top' id='brian'>Brian Pruitt</a>
-            <a href='#projects'>Projects</a>
-            <a href='#about'>About</a>
-            <a href='#contact'>Contact</a>
+
+            {/** Dropdown for small screens */}
+            <div className='dropdown'>
+                <button onClick={toggle}>
+                    Hamburger
+                </button>
+                {dropdownOpen && 
+                    <div className='dropdownLinks'>
+                        <a href='#projects'>Projects</a>
+                        <a href='#about'>About</a>
+                        <a href='#contact'>Contact</a>
+                    </div>}
+            </div>
+
+            {/** Fully displayed navbar for med to large screens */}
+            <div className='pageLinks'>
+                <a href='#projects'>Projects</a>
+                <a href='#about'>About</a>
+                <a href='#contact'>Contact</a>
+            </div>
         </div>
     );
 };
