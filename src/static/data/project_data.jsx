d2c779a7e497c7337694f520d175f05763927b69
Brian Pruitt <brianrpruitt63@gmail.com>
2023-04-12 19:57:15 -0400

    updated project descriptions

--- src/static/data/project_data.jsx
+++ src/static/data/project_data.jsx
@@ -8,7 +8,25 @@ export const projects = [
     {
         title: 'Sporty',
         image: {src: sporty, alt: 'Sporty Screenshot'},
-        description: 'description',
+        description: <>
+                        <p>
+                            Automated game scheduling dashboard that algorithmically 
+                            builds schedule for YMCA youth sports league seasons.  
+                            Made twice as fast the process of scheduling games, 
+                            tracking results, ranking teams, and seeding tournaments.
+                        </p>
+                        <p>
+                            React frontend, Node.js backend, PostgreSQL database.  
+                            React-bootstrap styling
+                        </p>
+                        <p>
+                            <span className='underlined'>
+                                Demo credentials
+                            </span><br/>
+                            Email: demo@demo.com<br/>
+                            Password: demo1234
+                        </p>
+                    </>,
         link: 'https://sporto-app.herokuapp.com/',
         video: 'https://www.loom.com/embed/befca497d2c443a2bec3e377b9e41c13',
         backendFiles: 'https://github.com/bpruitt63/sporty-backend',
@@ -45,7 +63,26 @@ export const projects = [
     {
         title: 'Boatey',
         image: {src: boatey, alt: 'Boatey Screenshot'},
-        description: 'description',
+        description: <>
+                        <p>
+                            Dashboard for planning boat trips. Interacts with 
+                            Marinas.com API to find local points of interest, 
+                            Mapbox API to display local area maps and calculate 
+                            distance between destinations, and Weatherbit API 
+                            to display local weather.
+                        </p>
+                        <p>
+                            React frontend, Node.js backend, PostgreSQL database.  
+                            Reactstrap styling
+                        </p>
+                        <p>
+                            <span className='underlined'>
+                                Demo credentials
+                            </span><br/>
+                            Email: demo@demo.com<br/>
+                            Password: demo1234
+                        </p>
+                    </>,
         link: 'https://boatey.herokuapp.com/',
         video: 'https://www.loom.com/embed/38885b03926643e8a1e868a2ccd8fc7a',
         backendFiles: 'https://github.com/bpruitt63/capstone2-backend',
@@ -54,7 +91,26 @@ export const projects = [
     {
         title: 'Gamey',
         image: {src: gamey, alt: 'Gamey Screenshot'},
-        description: 'description',
+        description: <>
+                        <p>
+                            Web app for gamers. Interacts with RAWG API to find 
+                            data on specific video games. Users can review games, 
+                            ask and answer questions, and upvote/downvote reviews 
+                            and answers.
+                        </p>
+                        <p>
+                            Flask with Python, Jinja, Flask SQLAlchemy, Flask 
+                            WTForms, Flask Bcrypt, JavaScript with axios, and 
+                            PostgreSQL database.
+                        </p>
+                        <p>
+                            <span className='underlined'>
+                                Demo credentials
+                            </span><br/>
+                            Email: demo@demo.com<br/>
+                            Password: demo1234
+                        </p>
+                    </>,
         link: 'https://gamey-project.herokuapp.com/',
         video: 'https://www.loom.com/embed/2dd8d96dce2c49a9b9c109c1f62d6511',
         singleRepo: 'https://github.com/bpruitt63/capstone1'
