5ec44c0e5563d9f9c4d730623978ff4c9ae22c11
Brian Pruitt <brianrpruitt63@gmail.com>
2023-04-12 20:07:42 -0400

    added resume link

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -9,10 +9,22 @@ function Contact() {
             <div id='contact' className='pageAnchor'></div>
             <h2 className='sectionTitle'>Contact</h2>
             <div className='contact'>
-                <a href={`mailto:${aboutMe.email}`}>{aboutMe.email}</a>
-                <a href={aboutMe.linkedIn}>LinkedIn</a>
-                <p>resume</p>
-                <a href={aboutMe.github}>GitHub</a>
+                <a href={`mailto:${aboutMe.email}`}
+                    target='blank'>
+                        {aboutMe.email}
+                </a>
+                <a href={aboutMe.linkedIn}
+                    target='blank'>
+                    LinkedIn
+                </a>
+                <a href='/src/brian_pruitt_resume.pdf'
+                    target='blank'>
+                    Resume
+                </a>
+                <a href={aboutMe.github}
+                    target='blank'>
+                    GitHub
+                </a>
             </div>
         </>
     );
