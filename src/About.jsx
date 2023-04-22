c3a70dccc904f1e03b26976c4476205dc6dd41ac
Brian Pruitt <brianrpruitt63@gmail.com>
2023-04-22 17:26:10 -0400

    updated about section

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './static/styles/about.css';
-import { aboutMe } from './static/data/about_me';
+import { aboutMe, mySkills } from './static/data/about_me';
 
 function About() {
 
@@ -8,7 +8,20 @@ function About() {
         <>
             <div id='about' className='pageAnchor'></div>
             <h2 className='sectionTitle'>About me</h2>
-            <div className='aboutMe'>{aboutMe}</div>
+            <div className='about'>
+                <div className='aboutMe'>{aboutMe}</div>
+                <div className='aboutImage'></div>
+            </div>
+            <div className='mySkills'>
+                <h3>My Skills</h3>
+                <ul>
+                    {Object.keys(mySkills).map(s =>
+                        <li key={s}>
+                            <span className='skill'>{`${s}: `}</span> 
+                            {mySkills[s]}
+                        </li>)}
+                </ul>
+            </div>
         </>
     );
 };
--- src/static/data/about_me.jsx
+++ src/static/data/about_me.jsx
@@ -1,14 +1,28 @@
 export const aboutMe = <>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing 
-                                elit. Phasellus consectetur, lacus placerat sollicitudin 
-                                pretium, tellus libero malesuada ligula, quis volutpat 
-                                lorem est quis est. Curabitur magna lorem, semper eu magna 
-                                non, rhoncus maximus sapien. Ut urna sapien, aliquam in 
-                                accumsan sit amet, rhoncus quis neque. Sed convallis quam 
-                                vitae placerat fermentum. Mauris at mattis diam, vitae 
-                                sollicitudin arcu. Aliquam a nunc commodo, fringilla orci 
-                                sed, consectetur metus. Praesent et egestas urna. Vestibulum 
-                                ac ligula vitae metus elementum suscipit in ut est.
+                                I am a tenacious software developer who doesn't 
+                                put down a problem until I find the best possible 
+                                solution. After several years as an accomplished 
+                                YMCA Program Director (and two time Interim CEO), 
+                                more than doubling the Y's program offerings and 
+                                participation during my tenure, I decided to take 
+                                on the challenge of transitioning my software 
+                                programming hobby into a career, giving me 
+                                a greater opportunity to use my creativity 
+                                and problem solving skills to create new programs 
+                                that can be used by people for a long time to come.
+                            </p>
+                            <p>
+                                When I'm not coding, you can usually find me on my
+                                boat as long as weather permits.  Other hobbies
+                                include lifting weights and playing video games,
+                                particularly in the strategy, open world, and RPG 
+                                genres.
                             </p>
                         </>
+
+export const mySkills = {'Front End': 'JavaScript, HTML, CSS, React, Bootstrap, JavaScript',
+                        'Back End': 'Node.js, Express, Python, Flask, REST, JSON',
+                        'Database': 'SQL, PostgreSQL',
+                        'Testing': 'Jest, Supertest, Jasmine',
+                        'Development and Deployment': 'Git, GitHub, Heroku, Vite'}
